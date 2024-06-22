7676c298e70f54a05bc9356dcd44f863b60f2b48
iskandar_norboyev <138550105+iskandarnorboyev@users.noreply.github.com>
2024-06-22 20:06:40 +0500

    Add files via upload

---
+++ users/views.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+from rest_framework.decorators import permission_classes
+from rest_framework.generics import CreateAPIView
+
+from .serializers import SignUpSerializer
+from .models import User
+
+
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny, )
+    serializer_class = SignUpSerializer
