2ca70a5e452ca50c5159977197c5b91591ff612b
iskandar_norboyev <i.norboyev309@gmail.com>
2024-09-02 06:10:37 +0500

    Your commit message

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import CreateUserView, VerifyAPIView, GetNewVerification, ChangeUserInformationView, ChangeUserPhotoView, LoginView, LoginRefreshView, LogOutView
+from .views import CreateUserView, VerifyAPIView, GetNewVerification, \
+    ChangeUserInformationView, ChangeUserPhotoView, LoginView, \
+    LoginRefreshView, LogOutView, ForgotPasswordView, ResetPasswordView
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
@@ -10,4 +12,6 @@ urlpatterns = [
     path('new-verify/', GetNewVerification.as_view()),
     path('change-user/', ChangeUserInformationView.as_view()),
     path('change-user-photo/', ChangeUserPhotoView.as_view()),
+    path('forgot-password/', ForgotPasswordView.as_view()),
+    path('reset-password/', ResetPasswordView.as_view()),
 ]
